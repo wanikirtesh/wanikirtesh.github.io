db4eada819f660cc634dab13d4113752da046aee
Kirtesh <wanikirtesh@gmail.com>
2015-08-09 20:39:21 +0530

    Adding pages

--- resources/common/common.js
+++ resources/common/common.js
@@ -8,3 +8,15 @@ function generateRandomNumber(maxLimit){
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 }
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
