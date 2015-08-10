85c80301a4d6c22a753e3eee7e42afe6483cd870
Kirtesh Wani <wanikirtesh@outlook.com>
2015-08-10 16:37:55 +0530

    Adding pages

--- resources/popup/quest.js
+++ resources/popup/quest.js
@@ -1,3 +1,47 @@
+		var myRnd;
+		var que;
+		var ans;
+		function validateAnswer(){
+			if($("#ans").val()==ans){
+				
+				if(typeof getUrlVars()["ans"] === 'undefined'){
+					$("#nxa").html("<h1>Congratulations!!!</h1>");
+				}else{
+				$("#nxa").html(getQuest()[parseInt(getUrlVars()["ans"])].answer);
+				}
+			}
+			else{
+				$("#nxa").html("<span style='color:red'>!!!Answer is not correct!!!</span>");
+			}
+		}
+		
+    	$(function() {
+			myRnd =  parseInt(getRandomArbitrary(0,10));
+			que = getQuest()[myRnd].question;
+			ans = getQuest()[myRnd].answer;
+			var count = parseInt(getUrlVars()["va"]);
+			if(isNaN(count)){
+			   count = parseInt(getRandomArbitrary(2,10));
+			   
+			}else{
+				//$("#kount").html("<h3>There will more "+(count-1)+"</h3>");
+			}
+			 
+			if(count<=1){
+			//alert(getUrlVars()["ans"]);
+				$("#kount").html("<h1>Answre is: " + getQuest()[parseInt(getUrlVars()["ans"])].answer + "</h1>");
+				$("#form-example").html("");
+			 }else{
+				$('#mq').html('<h1>' + que + '</h1>')
+			 }
+    		
+			$('a').click(function (event) {
+				event.preventDefault();
+				window.open("index.html?va="+(--count)+"&ans="+myRnd, "popupWindow"+(--count), "width=600, height=400, scrollbars=yes");
+			});
+    	})
+
+
 function getQuest(){
 	var text = '['+
 	'{"question":"Which scientist discovered the radioactive element radium?","answer":"Marie Curie"},' +
