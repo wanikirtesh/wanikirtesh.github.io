e8477337cf2faeff9763253040bbeefd509383d7
Kirtesh Wani <kirtesh.wani@ideas.com>
2019-11-11 12:36:32 +0530

    init

---
+++ ideas/resources/rpt.js
@@ -0,0 +1,47 @@
+var tbl;
+var filteredData;
+var columns=[];
+$(document).ready(function(){
+	$.urlParam = function (name) {
+		var results = new RegExp('[\?&]' + name + '=([^&#]*)')
+						  .exec(window.location.search);
+		return decodeURIComponent((results !== null) ? results[1] || 0 : false);
+	}
+	$.getJSON("./resources/data.json?t="+Date.now(),
+	    function(data){
+			filteredData = data.employess;
+			var dm;
+			Object.keys(filteredData[0]).forEach(key => {
+				var col = {};
+				col.data = key;
+				col.title = key.replace("_"," ").trim()
+				columns.push(col);
+				dm = Object.assign({},col); //important
+			})
+			dm.title="S.No";
+			columns.splice(0,0,dm)
+			
+			var filters = $.urlParam("fltlist").split(",");
+			for(var i =0;i<filters.length-1;i++){
+				console.log($.urlParam(filters[i]))
+				var ref_ = $.urlParam(filters[i]).split(",")
+				filteredData = filteredData.filter(x => ref_.includes(x[filters[i]].toString()))
+				
+			}
+			tbl = $("#rpt").DataTable({
+				"data":filteredData,
+				"columns":columns,
+				"columnDefs": [ {
+					"searchable": false,
+					"orderable": false,
+					"targets": 0
+				} ],
+				"order": [[ 1, 'asc' ]]
+			})
+			tbl.on( 'order.dt search.dt', function () {
+				tbl.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
+					cell.innerHTML = i+1;
+				} );
+			} ).draw();
+	});
+})
---
+++ ideas/resources/script.js
@@ -0,0 +1,106 @@
+var filtersData ={};
+var selections = [];
+var temp="";
+var fullData;
+var rnd_ = ""+Date.now();
+$(document).ready(function(){
+	var docHeight = $(document).height();
+	$("body").append("<div id='overlay' class='text-center my-auto'><div style='position:relative;top:50%' class='spinner-grow text-primary m-5 spinner-grow-lg' role='status' ><span class='sr-only'>Loading...</span></div></div>");
+	$("#overlay")
+	   .height(docHeight)
+	   .css({
+		  'opacity' : 0.4,
+		  'position': 'absolute',
+		  'top': 0,
+		  'left': 0,
+		  'background-color': 'black',
+		  'width': '100%',
+		  'z-index': 5000
+	});
+	$.getJSON("./resources/data.json?t="+Date.now(),
+		function(data){
+			fullData = data.employess;
+			Object.keys(fullData[0]).forEach(element => {
+				if(element.indexOf("_")!=0){
+					var optionText = element.replace("_"," ")
+					$('#selDetails').append(new Option(optionText, element))
+					filtersData[element] = new Set(data.employess.map(x => x[element]).sort());
+				}
+			});
+			$("#overlay").css("display","none");
+		});
+
+	$('#selDetails').change(function(){
+		$("#overlay").css("display","block");
+		temp = $(this).val()
+		setTimeout(function(){updateFilterPan(temp);
+			if(selections.length>0)
+			$('#btnGen').prop( "disabled", false );
+		else
+			$('#btnGen').prop( "disabled", true );
+		},getRandomInt(1000,10000));
+	})
+
+	$('#btnGen').click(function(e){
+		e.preventDefault();
+		var filteredData = "?fltlist=";
+		var fltString="";
+		$('.cfilter select').each(function(index){
+			var colname = $(this).attr("id").replace("flt","");
+			filteredData +=colname + ",";
+			fltString += (colname + "=" + $(this).val().join(",") + "&");
+		})
+		filteredData += ("&" + fltString);
+		setTimeout(function(){window.open("./report.html" + filteredData , "popupWindow", "width=800,resizable=false,height=650,scrollbars=yes")},getRandomInt(1000,3000));
+	})
+})
+
+function updateFilterPan(filters){
+	filters.forEach(element => {
+		if(!selections.includes(element)){
+			selections.push(element);
+			addFilter(element);
+		}
+		$("#overlay").css("display","none");
+	});
+	var t = selections.length;
+	for(var i=0;i<t;i++){
+		var element=selections[i];
+		if(element!=undefined && !filters.includes(element)){
+			removeSelection(element);
+			removeFilter(element);
+			i--;
+		}
+	}
+}
+
+function removeSelection(str){
+	for(var i =0;i<selections.length;i++){
+		if(selections[i] == str){
+			selections.splice(i,1);
+			break;
+		}
+	}
+}
+
+function addFilter(strFilterName){
+	var strFilterName_ = strFilterName.replace(' ','_').toLowerCase()
+	var optList = '<select class="form-control" multiple id="flt'+strFilterName_+'">';
+	filtersData[strFilterName_].forEach(element=>{
+		optList += '<option selected>'+element+'</option>';
+	})
+	optList += '</select>'
+	$(".cfilter").append('<div class="col-md-4" id="fltr'+strFilterName_+'"><label for="flt'+strFilterName_+'">'+strFilterName.replace("_"," ")+':</label>'+optList+'</div>');
+
+}
+
+function removeFilter(strFilterName){
+	var strFilterName_ = 'fltr'+strFilterName.replace(' ','_').toLowerCase()
+	$(document.getElementById(strFilterName_)).remove();
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
